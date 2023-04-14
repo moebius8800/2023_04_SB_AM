b133b48c25a148854f87239319cf320cc4128536
moebius8800 <moebius8800@gmail.com>
2023-04-14 14:53:15 +0900

    writeArticle

--- src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
@@ -11,35 +11,53 @@ import com.koreaIT.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-
+	
 	private int lastArticleId;
 	private List<Article> articles;
-
+	
 	public UsrArticleController(){
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+	
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+	}
+	
+	private Article writeArticle(String title, String body) {
+		int id = this.lastArticleId + 1;
+		this.lastArticleId = id;
+		
+		Article article = new Article(id, title, body);
+		
+		articles.add(article);
+		
+		return article;
 	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-
-		int id = this.lastArticleId + 1;
-		this.lastArticleId = id;
-
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-
+		
+		Article article = writeArticle(title, body);
+		
 		return article;
 	}
-
+	
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-
+		
 		return this.articles;
 	}
-
-
+	
+	
 }
